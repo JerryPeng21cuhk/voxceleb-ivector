4f974eb2d558395a89b4e770e816c7ca5c5f951d
JerryPeng21cuhk <jerrypeng1937@gmail.com>
2018-12-27 13:32:02 +0800

    revised

--- run.sh
+++ run.sh
@@ -5,44 +5,33 @@
 
 stage=0
 
-. cmd.sh
-. path.sh
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
 set -e
 mfccdir=`pwd`/mfcc
 vaddir=`pwd`/mfcc
-trials=data/voxceleb1_trials/voxceleb1_trials_sv
+trials=data/voxceleb1_trials_sv
 num_components=2048 # Larger than this doesn't make much of a difference.
 
 
 if [ $stage -le 0 ]; then
 # Preparing dataset folder voxceleb1. The voxceleb1 folder should have subdir voxceleb1_wav which contain wav files.
-./local/make_voxceleb1_sv.pl /data/sls/scratch/swshon/dataset/voxceleb1/ data
+./local/make_voxceleb1_sv.pl /lan/ibdata/SPEECH_DATABASE/voxceleb1 data
 
 # Extract speaker recogntion features.
-steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 100 --cmd "$train_cmd" \
-    data/voxceleb1_train exp/make_mfcc $mfccdir
-steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 100 --cmd "$train_cmd" \
-    data/voxceleb1_test exp/make_mfcc $mfccdir
-steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 100 --cmd "$train_cmd" \
-    data/voxceleb1_test_1utt exp/make_mfcc $mfccdir
-
-for name in voxceleb1_train voxceleb1_test voxceleb1_test_1utt; do
-  utils/fix_data_dir.sh data/${name}
-done
-
-fi
-
-if [ $stage -le 1 ]; then
+steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 30 --cmd "$train_cmd" \
+    data/train exp/make_mfcc $mfccdir
+steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 30 --cmd "$train_cmd" \
+    data/test exp/make_mfcc $mfccdir
 
 # VAD decision
 sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" \
-    data/voxceleb1_train exp/make_vad $vaddir
+    data/train exp/make_vad $vaddir
 sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" \
-    data/voxceleb1_test exp/make_vad $vaddir
-sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" \
-    data/voxceleb1_test_1utt exp/make_vad $vaddir
+    data/test exp/make_vad $vaddir
 
-for name in voxceleb1_train voxceleb1_test voxceleb1_test_1utt; do
+for name in train test; do
   utils/fix_data_dir.sh data/${name}
 done
 
@@ -53,17 +42,17 @@ fi
 if [ $stage -le 2 ]; then
 
 # Train UBM and i-vector extractor.
-sid/train_diag_ubm.sh --nj 40 --num-threads 8 --cmd "$train_cmd --mem 20G"\
-    data/voxceleb1_train $num_components \
+sid/train_diag_ubm.sh --nj 10 --num-threads 4 --cmd "$train_cmd --mem 10G"\
+    data/train $num_components \
     exp/diag_ubm_$num_components
 
-sid/train_full_ubm.sh --nj 40 --remove-low-count-gaussians false \
-    --cmd "$train_cmd --mem 25G" data/voxceleb1_train \
+sid/train_full_ubm.sh --nj 10 --remove-low-count-gaussians true \
+    --cmd "$train_cmd --mem 10G" data/train \
     exp/diag_ubm_$num_components exp/full_ubm_$num_components
 
-sid/train_ivector_extractor.sh --num-threads 7 --nj 20 --num_processes 2 --cmd "$train_cmd --mem 16G" \
+sid/train_ivector_extractor.sh --num-threads 4 --nj 10 --num_processes 1 --cmd "$train_cmd --mem 10G" \
   --ivector-dim 600 \
-  --num-iters 5 exp/full_ubm_$num_components/final.ubm data/voxceleb1_train \
+  --num-iters 5 exp/full_ubm_$num_components/final.ubm data/train \
   exp/extractor
 
 fi
@@ -71,17 +60,13 @@ fi
 if [ $stage -le 3 ]; then
 
 # Extract i-vectors.
-sid/extract_ivectors.sh --cmd "$train_cmd --mem 6G " --nj 300 \
-   exp/extractor data/voxceleb1_train \
-   exp/ivectors_voxceleb1_train
+sid/extract_ivectors.sh --cmd "$train_cmd --mem 6G " --nj 30 \
+   exp/extractor data/train \
+   exp/ivectors_train
 
-sid/extract_ivectors.sh --cmd "$train_cmd --mem 6G " --nj 40 \
-   exp/extractor data/voxceleb1_test \
-   exp/ivectors_voxceleb1_test
-
-sid/extract_ivectors.sh --cmd "$train_cmd --mem 6G " --nj 100 \
-   exp/extractor data/voxceleb1_test_1utt \
-   exp/ivectors_voxceleb1_test_1utt
+sid/extract_ivectors.sh --cmd "$train_cmd --mem 6G " --nj 30 \
+   exp/extractor data/test \
+   exp/ivectors_test
 
 fi
 
@@ -90,34 +75,34 @@ fi
 if [ $stage -le 4 ]; then
 
 # cosine distance scoring
-local/cosine_scoring.sh data/voxceleb1_test_1utt data/voxceleb1_test_1utt \
-  exp/ivectors_voxceleb1_test_1utt exp/ivectors_voxceleb1_test_1utt $trials local/scores_voxceleb1
+local/cosine_scoring_pairwise.sh data/test data/test \
+  exp/ivectors_test exp/ivectors_test $trials local/scores
 
-eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores_voxceleb1/cosine_scores) 2> /dev/null`
+eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores/cosine_scores) 2> /dev/null`
 echo "CDS eer : $eer"
 
 # LDA+cosine distance scoring
-local/lda_scoring.sh data/voxceleb1_train data/voxceleb1_test_1utt data/voxceleb1_test_1utt \
-  exp/ivectors_voxceleb1_train exp/ivectors_voxceleb1_test_1utt exp/ivectors_voxceleb1_test_1utt $trials \
-  local/scores_voxceleb1
+local/lda_scoring_pairwise.sh data/train data/test data/test \
+  exp/ivectors_train exp/ivectors_test exp/ivectors_test $trials \
+  local/scores
 
-eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores_voxceleb1/lda_scores) 2> /dev/null`
+eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores/lda_scores) 2> /dev/null`
 echo "LDA+CDS eer : $eer"
 
 # PLDA scoring
-ivector-mean scp:exp/ivectors_voxceleb1_train/ivector.scp exp/ivectors_voxceleb1_train/mean.vec
-local/plda_scoring.sh data/voxceleb1_train data/voxceleb1_test_1utt data/voxceleb1_test_1utt \
-  exp/ivectors_voxceleb1_train exp/ivectors_voxceleb1_test_1utt exp/ivectors_voxceleb1_test_1utt \
-  $trials local/scores_voxceleb1
+ivector-mean scp:exp/ivectors_train/ivector.scp exp/ivectors_train/mean.vec
+local/plda_scoring_pairwise.sh data/train data/test data/test \
+  exp/ivectors_train exp/ivectors_test exp/ivectors_test \
+  $trials local/scores
 
-eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores_voxceleb1/plda_scores) 2> /dev/null`
+eer=`compute-eer <(python local/prepare_for_eer.py $trials local/scores/plda_scores) 2> /dev/null`
 echo "PLDA eer : $eer"
 
 fi
 
 
-#GMM-2048 CDS eer : 15.39
-#GMM-2048 LDA+CDS eer : 8.103
-#GMM-2048 PLDA eer : 5.446
+#GMM-2048 CDS eer : 15.6
+#GMM-2048 LDA+CDS eer : 7.937
+#GMM-2048 PLDA eer : 5.652
 
 
